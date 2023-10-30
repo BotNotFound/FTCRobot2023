a52626612a12b7e6a28590faf3698918b1648d75
FreehandBlock51 <60456293+FreehandBlock51@users.noreply.github.com>
2023-10-30 16:07:42 -0700

    fixed a bug relating to not changing remainingDistance & made code thread-safe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PositionalDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PositionalDriveTrain.java
@@ -14,38 +14,49 @@ public final class PositionalDriveTrain extends DriveTrain {
     private final Thread positionUpdaterThread = new Thread(() -> {
         double prevTime = parent.getRuntime();
         double deltaTime;
+        Point threadSafeRemainingDistance, distanceOffset;
 
         while (!killUpdaterThread) {
             deltaTime = parent.getRuntime() - prevTime;
             prevTime = parent.getRuntime();
 
+            synchronized (remainingDistance) {
+                threadSafeRemainingDistance = remainingDistance;
+            }
+
             // rotate, then move
-            if (remainingDistance.rotation != 0) {
-                double absRemainingRotation = Math.abs(remainingDistance.rotation);
+            if (threadSafeRemainingDistance.rotation != 0) {
+                double absRemainingRotation = Math.abs(threadSafeRemainingDistance.rotation);
                 // (same w/ x & y, distance is measured in nanoseconds) if deltaTime > remaining
                 //  distance, set motors to fractional power; otherwise, just set to full power
-                if (remainingDistance.rotation < deltaTime) {
-                    setVelocity(0, 0, Math.copySign(absRemainingRotation / deltaTime, remainingDistance.rotation));
-                    remainingDistance.rotation = 0;
+                if (threadSafeRemainingDistance.rotation < deltaTime) {
+                    setVelocity(0, 0, Math.copySign(absRemainingRotation / deltaTime, threadSafeRemainingDistance.rotation));
+                    distanceOffset = new Point(0,0,-threadSafeRemainingDistance.rotation);
                 }
                 else {
-                    setVelocity(0, 0, Math.copySign(1, remainingDistance.rotation));
-                    remainingDistance.rotation -= Math.copySign(deltaTime, remainingDistance.rotation);
+                    setVelocity(0, 0, Math.copySign(1, threadSafeRemainingDistance.rotation));
+                    distanceOffset = new Point(0,0,-Math.copySign(deltaTime, threadSafeRemainingDistance.rotation));
                 }
             }
-            else if (remainingDistance.x != 0 || remainingDistance.y != 0) {
-                double absRemainingDistX = Math.abs(remainingDistance.x);
-                double absRemainingDistY = Math.abs(remainingDistance.y);
-                double powerX = Math.copySign(absRemainingDistX < deltaTime ? absRemainingDistX / deltaTime : 1, remainingDistance.x);
-                double powerY = Math.copySign(absRemainingDistY < deltaTime ? absRemainingDistY / deltaTime : 1, remainingDistance.y);
+            else if (threadSafeRemainingDistance.x != 0 || threadSafeRemainingDistance.y != 0) {
+                double absRemainingDistX = Math.abs(threadSafeRemainingDistance.x);
+                double absRemainingDistY = Math.abs(threadSafeRemainingDistance.y);
+                double powerX = Math.copySign(absRemainingDistX < deltaTime ? absRemainingDistX / deltaTime : 1, threadSafeRemainingDistance.x);
+                double powerY = Math.copySign(absRemainingDistY < deltaTime ? absRemainingDistY / deltaTime : 1, threadSafeRemainingDistance.y);
                 setVelocity(powerX, powerY, 0);
-                remainingDistance.subtract(new Point(Math.copySign(Math.max(absRemainingDistX, deltaTime), remainingDistance.x),
-                        Math.copySign(Math.max(absRemainingDistY, deltaTime), remainingDistance.y),
-                        0));
+                distanceOffset = new Point(Math.copySign(Math.max(absRemainingDistX, deltaTime), threadSafeRemainingDistance.x),
+                        Math.copySign(Math.max(absRemainingDistY, deltaTime), threadSafeRemainingDistance.y),
+                        0).negate();
             }
             else {
                 setVelocity(0, 0, 0);
+                distanceOffset = new Point(0,0,0);
             }
+
+            synchronized (remainingDistance) {
+                remainingDistance = remainingDistance.add(distanceOffset);
+            }
+            try { Thread.sleep(10); } catch (InterruptedException e) {} // arbitrary offset
         }
     });
 

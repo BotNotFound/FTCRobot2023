66c7fde7a37607c7c1a0c401ab766826be86bc53
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-03 20:30:21 -0700

    fixed drive train input

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMain.java
@@ -39,8 +39,8 @@ public class TeleOpMain extends OpBase {
 
         // 1st gamepad controls movement
         driveTrain.setVelocity(
-                gamepad1.left_stick_y,
-                -gamepad1.left_stick_x,
+                gamepad1.left_stick_x,
+                -gamepad1.left_stick_y,
                 gamepad1.right_stick_x
         );
 

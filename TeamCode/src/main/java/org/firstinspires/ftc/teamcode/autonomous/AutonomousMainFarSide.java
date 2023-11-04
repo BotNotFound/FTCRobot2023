b67a00dc1d62cbbcf79ab9e05a704e9a8828e3d5
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-04 10:19:38 -0700

    added AutonomousMainFarSide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.OpBaseLinear;
+
+@Autonomous(name = "Autonomous Control (Non-Backdrop Side)")
+public class AutonomousMainFarSide extends OpBaseLinear {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveTrain.setVelocity(0,1,0);
+        Thread.sleep(3000, 0);
+        driveTrain.setVelocity(0,0,0);
+    }
+    
+}

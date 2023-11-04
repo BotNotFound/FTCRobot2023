29bfa2c5832a9c940c2390b99902cc55cfa755fc
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-04 11:12:30 -0700

    added delay to AutonomousMainFarSide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
@@ -9,6 +9,7 @@ public class AutonomousMainFarSide extends OpBaseLinear {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        Thread.sleep(5000, 0);
         driveTrain.setVelocity(0,1,0);
         Thread.sleep(3000, 0);
         driveTrain.setVelocity(0,0,0);

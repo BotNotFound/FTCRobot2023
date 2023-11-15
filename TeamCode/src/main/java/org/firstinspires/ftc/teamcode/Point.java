1f9fd518d97b7db2cb97ee35f02e74452dd4fec8
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-14 17:30:17 -0800

    Fancy stuff for Point.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Point.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Point.java
@@ -1,12 +1,59 @@
 package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 /**
  * A point.  The coordinate system we are using is horizontal and vertical translation in a rotated
  *  XY coordinate plane, and our unit of distance is nanoseconds.
  */
 public final class Point {
+    public static final double EPSILON = 0.001;
+
+    /**
+     * Determines if the point is small enough to be considered 'zero'
+     * @return true if the point is small enough
+     */
+    public boolean isZero() {
+        return Math.abs(x) < EPSILON &&
+                Math.abs(y) < EPSILON &&
+                Math.abs(rotation) < EPSILON;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != Point.class) {
+            return false;
+        }
+        return negate().add((Point)obj).isZero();
+    }
+
+    /**
+     * The number of bits in an integer. Used by hashCode.
+     * @see #hashCode()
+     */
+    private static final int LENGTH_OF_AN_INTEGER = (int)(Math.log(Integer.MAX_VALUE) / Math.log(2)) + 1; // MAX_VALUE is positive, so it will be 1 bit short
+    /**
+     * A bitmask for the first third of an integer. Used by hashCode.
+     * @see #hashCode()
+     */
+    private static final int THIRD_INT_MASK = (Integer.MAX_VALUE << (2 * LENGTH_OF_AN_INTEGER / 3)) >> (2 * LENGTH_OF_AN_INTEGER / 3);
+
+    @Override
+    public int hashCode() {
+        // only care about equality as precise as EPSILON
+        long roundedX = Math.round(x / EPSILON);
+        long roundedY = Math.round(y / EPSILON);
+        long roundedRotation = Math.round(rotation / EPSILON);
+        return (int)(
+                ((roundedX & THIRD_INT_MASK) << ((2 * LENGTH_OF_AN_INTEGER) / 3)) |
+                        (roundedY | THIRD_INT_MASK) << (LENGTH_OF_AN_INTEGER / 3) |
+                        (roundedRotation | THIRD_INT_MASK)
+        ); // unique enough for our nonexistent usage of this function
+    }
 
     public enum Axis {
         X,

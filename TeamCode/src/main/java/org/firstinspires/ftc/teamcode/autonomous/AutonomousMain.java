fe3923c1398f7df61cdc89c44b235d8ce2541e4d
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-24 13:10:49 -0800

    fixed AutonomousMain referencing nonexistent modules

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMain.java
@@ -3,16 +3,9 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.OpBaseLinear;
-import org.firstinspires.ftc.teamcode.modules.computervision.testing.ColorMassDetectionProcessor;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.opencv.core.Scalar;
 
 @Autonomous(name = "Autonomous Control")
 public class AutonomousMain extends OpBaseLinear {
-
-    private ColorMassDetectionProcessor cmdProcessor = new ColorMassDetectionProcessor(new Scalar(0, 0, 150), new Scalar(180, 72, 255), () -> 50, () -> 426, () -> 853);
-    private VisionPortal portal;
-
     @Override
     public void runOpMode() throws InterruptedException {
         driveTrain.setVelocity(0,1,0);

aa56eb87bf467d867bb20dded477e457f9ce4cd5
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-24 12:45:38 -0800

    Fixed typos in AprilTagLocator.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/AprilTagLocator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/AprilTagLocator.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+import androidx.annotation.NonNull;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
 import org.firstinspires.ftc.teamcode.Movement;
 import org.firstinspires.ftc.teamcode.modules.location.LocalizedMovement;
 import org.firstinspires.ftc.teamcode.modules.location.Locator;
@@ -16,11 +18,6 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import androidx.annotation.NonNull;
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 public class AprilTagLocator extends ModuleBase implements Locator {
 
@@ -63,7 +60,7 @@ public class AprilTagLocator extends ModuleBase implements Locator {
 
         // Create the vision portal the easy way.
             visionPortal = VisionPortal.easyCreateWithDefaults(
-                    hardwareMap.get(WebcamName.class, "Webcam1"), aprilTag);
+                    parent.hardwareMap.get(Camera.class, "Webcam1").getCameraName(), aprilTag);
     }
 
 
@@ -72,25 +69,25 @@ public class AprilTagLocator extends ModuleBase implements Locator {
      */
     private void telemetryAprilTag() {
         List<org.firstinspires.ftc.vision.apriltag.AprilTagDetection> currentDetections = aprilTag.getDetections();
-        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        getTelemetry().addData("# AprilTags Detected", currentDetections.size());
 
         // Step through the list of detections and display info for each one.
         for (org.firstinspires.ftc.vision.apriltag.AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
-                telemetry.addLine(String.format(Locale.US, "\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format(Locale.US, "XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                telemetry.addLine(String.format(Locale.US, "PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format(Locale.US, "RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                getTelemetry().addLine(String.format(Locale.US, "\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                getTelemetry().addLine(String.format(Locale.US, "XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                getTelemetry().addLine(String.format(Locale.US, "PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                getTelemetry().addLine(String.format(Locale.US, "RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
             } else {
-                telemetry.addLine(String.format(Locale.US, "\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format(Locale.US, "Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                getTelemetry().addLine(String.format(Locale.US, "\n==== (ID %d) Unknown", detection.id));
+                getTelemetry().addLine(String.format(Locale.US, "Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
             }
         }   // end for() loop
 
         // Add "key" information to telemetry
-        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
-        telemetry.addLine("RBE = Range, Bearing & Elevation");
+        getTelemetry().addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        getTelemetry().addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        getTelemetry().addLine("RBE = Range, Bearing & Elevation");
 
     }   // end method telemetryAprilTag()
 

ac7dc2f8b156f8b560107e7538a205f6ae81deec
FreehandBlock51 <freehandblock51@outlook.com>
2023-11-28 18:14:14 -0800

    skeleton AutonomousMainReal updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainReal.java
@@ -10,10 +10,10 @@ import org.firstinspires.ftc.teamcode.modules.location.AprilTagLocator;
 import org.firstinspires.ftc.teamcode.modules.location.LocalizedMovement;
 
 @Autonomous(name = "Autonomous Main (Drive To Position)")
-public final class AutonomousMainReal extends OpBaseLinear {
+public class AutonomousMainReal extends OpBaseLinear {
     private AprilTagLocator aprilTagLocator;
 
-    enum Side { LEFT, MIDDLE, RIGHT }
+    public static final double DISTANCE_TO_TEAM_PROP_U = 50.0;
 
     @Override
     public void initHardware() throws InterruptedException {
@@ -23,23 +23,36 @@ public final class AutonomousMainReal extends OpBaseLinear {
 
     @Override
     public void runOpMode() {
-        Side side;
-
+        // idle
         arm.extendTo(LinearSlide.Presets.IDLE);
         arm.rotateJoint(Arm.Presets.IDLE);
 
-        // use location of team prop to determine path
-        side = Side.MIDDLE;
-        aprilTagLocator.setTagId(5); // TODO when vision is done, do that
+        driveTrain.driveToOdometry(Movement.Axis.Y.genPointFromAxis(DISTANCE_TO_TEAM_PROP_U)); // drive to the U
 
-        driveTrain.driveToOdometry(new Movement(0, 5, 0)); // TODO Vision locator goes here
+        // determine what side team prop is on
+        final double leftConfidence, middleConfidence, rightConfidence;
+        middleConfidence = 3; // TODO check w/ vision module
+        driveTrain.driveToOdometry(Movement.Axis.THETA.genPointFromAxis(-Math.PI / 2));
+        leftConfidence = 0; // TODO see above
+        driveTrain.driveToOdometry(Movement.Axis.THETA.genPointFromAxis(Math.PI));
+        rightConfidence = 0; // TODO see above
+
+        if (middleConfidence > leftConfidence && middleConfidence > rightConfidence) { // middle is most confident
+            aprilTagLocator.setTagId(5);
+        }
+        else if (leftConfidence > rightConfidence) { // left is most confident, or equal with middle
+            aprilTagLocator.setTagId(4);
+        }
+        else { // right is most confident, or all are equal
+            aprilTagLocator.setTagId(6);
+        }
+
+        // use location of team prop to determine path
+        driveTrain.driveToOdometry(driveTrain.getLocation().add(Movement.Axis.THETA.genPointFromAxis(-Math.PI / 2)));
 
         driveTrain.driveToOdometry(new Movement(0, 0, Math.PI / 2)); // rotate so we can see the AprilTags
         // AprilTagLocator only works if we can see the April Tag, So we convert its location to a more reliable locator
         LocalizedMovement aprilTagLoc_odom = new LocalizedMovement(0, 1, aprilTagLocator).convertToOtherLocator(driveTrain);
-        if (side == Side.RIGHT) {
-            // TODO don't run into the team prop
-        }
         driveTrain.driveTo(aprilTagLoc_odom);
 
         arm.extendTo(LinearSlide.Presets.ONE_SLIDE_USED);

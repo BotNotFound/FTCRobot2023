e736ea40b6d0fd42658bdb073c0bd4715e6b361b
FreehandBlock51 <freehandblock51@outlook.com>
2023-12-02 08:14:17 -0800

    update autos to release claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMain.java
@@ -3,14 +3,17 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.OpBaseLinear;
+import org.firstinspires.ftc.teamcode.modules.DoubleClaw;
+import org.firstinspires.ftc.teamcode.modules.DriveTrain;
 
 @Autonomous(name = "Autonomous Control")
 public class AutonomousMain extends OpBaseLinear {
     @Override
     public void runOpMode() throws InterruptedException {
-        driveTrain.setVelocity(0,1,0);
+        ((DriveTrain)driveTrain).setVelocity(0,-1,0);
         Thread.sleep(1000, 0);
         driveTrain.setVelocity(0,0,0);
+        while (doubleClaw.getClawState() != DoubleClaw.ClawState.BOTH_RELEASED) doubleClaw.incrementClawState();
     }
     
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.OpBaseLinear;
+import org.firstinspires.ftc.teamcode.modules.DoubleClaw;
+import org.firstinspires.ftc.teamcode.modules.DriveTrain;
 
 @Autonomous(name = "Autonomous Control (Non-Backdrop Side)")
 public class AutonomousMainFarSide extends OpBaseLinear {
@@ -10,9 +12,10 @@ public class AutonomousMainFarSide extends OpBaseLinear {
     @Override
     public void runOpMode() throws InterruptedException {
         Thread.sleep(5000, 0);
-        driveTrain.setVelocity(0,1,0);
+        ((DriveTrain)driveTrain).setVelocity(0,-1,0);
         Thread.sleep(3000, 0);
         driveTrain.setVelocity(0,0,0);
+        while (doubleClaw.getClawState() != DoubleClaw.ClawState.BOTH_RELEASED) doubleClaw.incrementClawState();
     }
     
 }

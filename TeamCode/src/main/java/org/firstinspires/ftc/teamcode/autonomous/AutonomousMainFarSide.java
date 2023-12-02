f60eaf4167cd72dbb90dc6a36216ac89b90cca8b
FreehandBlock51 <freehandblock51@outlook.com>
2023-12-02 10:01:47 -0800

    made longer wait on AutonomousMainFarSide.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMainFarSide.java
@@ -9,7 +9,7 @@ public class AutonomousMainFarSide extends OpBaseLinear {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Thread.sleep(5000, 0);
+        Thread.sleep(20000, 0);
         driveTrain.resetRotation();
         driveTrain.setVelocity(0,1,0);
         Thread.sleep(3000, 0);

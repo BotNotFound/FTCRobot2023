babf624319f3d6cc2abb5c36d1c54462997fb19c
Asvaka <78432238+Asvaka@users.noreply.github.com>
2023-12-10 17:34:42 -0800

    Created a new PropDetector, this one using a Color Sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/detection/PropDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/detection/PropDetector.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.modules.detection;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+
 import org.firstinspires.ftc.teamcode.modules.ModuleBase;
 
 public class PropDetector extends ModuleBase {
@@ -20,7 +21,19 @@ public class PropDetector extends ModuleBase {
     }
 
     public boolean isPropDetected(Prop prop) {
-        return sensor.argb() == prop.argb;
+        /*float[] currentHSV = new float[3];
+        Color.RGBToHSV(sensor.red(), sensor.green(), sensor.blue(), currentHSV);
+        Color.RGBToHSV(prop.redChannel(), prop.greenChannel(), prop.blueChannel(), testHSV);
+        if(currentHSV[1] >= testHSV[1] && currentHSV[0] >= testHSV[0] - colorPreciseness && currentHSV[0] <= testHSV[0] + colorPreciseness) {
+            return true;
+        }*/
+        switch (prop) {
+            case RED_TEAM_PROP:
+                return sensor.red() > sensor.green() && sensor.red() > sensor.blue();
+            case BLUE_TEAM_PROP:
+                return sensor.blue() > sensor.green() && sensor.blue() > sensor.red();
+        }
+        return false;
     }
 
     @Override

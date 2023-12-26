8f1b8e57f52f2e243d693f4ae15991c14b0470fe
FreehandBlock51 <freehandblock51@outlook.com>
2023-12-26 13:47:40 -0800

    Active intake tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/ActiveIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/ActiveIntake.java
@@ -2,29 +2,57 @@ package org.firstinspires.ftc.teamcode.modules;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.hardware.ConditionalHardwareDevice;
 import org.firstinspires.ftc.teamcode.modules.core.Module;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ActiveIntake extends Module {
 	private final ConditionalHardwareDevice<DcMotor> intakeMotor;
 
 	public static final String INTAKE_MOTOR_NAME = "Intake Motor";
 
+	private double activePower = 1.0;
+
+	private final AtomicBoolean on;
+
 	public ActiveIntake(OpMode registrar) {
 			super(registrar);
+			on = new AtomicBoolean(false);
 			intakeMotor = ConditionalHardwareDevice.tryGetHardwareDevice(parent.hardwareMap, DcMotor.class, INTAKE_MOTOR_NAME);
+
+			intakeMotor.runIfAvailable(intake -> {
+				intake.setDirection(DcMotorSimple.Direction.REVERSE);
+			});
+	}
+
+	public void setPower(double newPower) {
+		activePower = newPower;
+	}
+
+	public double getPower() {
+		return activePower;
 	}
 
 	public void start() {
-		intakeMotor.runIfAvailable(intake ->
-			intake.setPower(-1.0)
-		);
+		if (on.compareAndSet(false, true)) {
+			intakeMotor.runIfAvailable(intake ->
+					intake.setPower(activePower)
+			);
+		}
 	}
 
 	public void stop() {
-		intakeMotor.runIfAvailable(intake ->
-				intake.setPower(0.0)
-		);
+		if (on.compareAndSet(true, false)) {
+			intakeMotor.runIfAvailable(intake ->
+					intake.setPower(0.0)
+			);
+		}
+	}
+
+	public boolean isActive() {
+		return on.get();
 	}
 
 	@Override
@@ -33,5 +61,10 @@ public class ActiveIntake extends Module {
 	}
 
 	@Override
-	public void log() {}
+	public void log() {
+		intakeMotor.runIfAvailable(dcMotor -> {
+			getTelemetry().addData("Is intake active", isActive());
+			getTelemetry().addData("Intake power", getPower());
+		});
+	}
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/ActiveIntakeTuner.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.opmode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.modules.ActiveIntake;
+import org.firstinspires.ftc.teamcode.modules.core.ModuleManager;
+
+@TeleOp(group = "Tests")
+public class ActiveIntakeTuner extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ModuleManager manager = new ModuleManager(this);
+        ActiveIntake in = manager.getModule(ActiveIntake.class);
+
+        waitForStart();
+
+        in.start();
+        while (opModeIsActive()) {
+            in.setPower(gamepad1.left_stick_y);
+            telemetry.addData("Current Power", in.getPower());
+        }
+        in.stop();
+    }
+}

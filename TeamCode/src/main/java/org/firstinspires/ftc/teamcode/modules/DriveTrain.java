e88b3171b837ef99e947e485e324da2fee05a45a
Asvaka <78432238+Asvaka@users.noreply.github.com>
2024-01-26 20:42:54 -0800

    Fixed weird jitter in control of FieldCentricDriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveTrain.java
@@ -158,8 +158,17 @@ public class DriveTrain extends Module {
             // (formula was found on gm0)
             double leftFrontPower = forward + strafe + rotation;
             double leftBackPower = forward - strafe + rotation;
-            double rightFrontPower = forward + strafe - rotation;
-            double rightBackPower = forward - strafe - rotation;
+            double rightFrontPower = forward - strafe - rotation;
+            double rightBackPower = forward + strafe - rotation;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            /*double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;*/
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.

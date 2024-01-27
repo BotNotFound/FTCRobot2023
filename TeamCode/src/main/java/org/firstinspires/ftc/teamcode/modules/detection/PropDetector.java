c24ac1acc69cdbbea7c0a0c934abb73f395c62f2
Asvaka <78432238+Asvaka@users.noreply.github.com>
2024-01-26 20:50:39 -0800

    Fixed FieldCentricDriveTrain and updated PropDetector (#26)
    * Color Thresholding Started
    
    * Added ColorMassDetectionProcessor which I got from someone on the FIRST robotics discord.
    
    * Implemented ColorMassDetectionProcessor in CameraTest
    
    * Set cmdProccesor constraints to include all colors for testing.
    
    * Added a SimColorMassDetectionProcessor to test hsv values with EOCV Sim. Also changed CameraTest ColorMassDetectionProcessor initial values to more ideal values.
    
    * Started implementation of ColorMassDetectionProcessor in AutonomousMain, and added Distance_finder to ColorMassDetectionProcessor
    
    * Implemented TensorFlowObjectDetection Locator
    
    * Fixed TeamPropLocator not working and throwing an error whenever it ran
    
    * Added a layer of abstraction to the TeamPropLocator by making a different prop locator for each of the props.
    
    * Changed model creation method, added the new models, and added a getSimpleLocation method to tell whether the prop is on the left, right, or center of the screen.
    
    * Added models, and changed the LABELS string array to work with them in the PropLocators. NOTE: This does not work, as the models are image classification, while the FTC SDK TFOD uses object classification
    
    * Created a new PropDetector, this one using a Color Sensor.
    
    * Cleaned up the PropDetector class and added comments
    
    * Cleaned up file directory and removed files from systems that are no longer and will never be used.
    
    * Updated PropDetector to work with ConditionalHardwareDevice
    
    * Removed an unnecessary comment from PropDetector
    
    * Added limiting to Wrist movement so it doesn't ram into the Arm
    
    * Fixed reference to previousGamepad2 when reference to previousGamepad1 was intended.
    
    * Overhauled flap system, changed it from a toggle system to a cycle system. Also updated all uses of toggle system to work with cycle system.
    
    * Light beautification of Arm.java
    
    * Updated PropDetector to also check distance, as the color sensor portion of the color range sensor has an effective range of 2-3 centimeters, while the distance sensor has an effective range of about 8 centimeters.
    
    * Fixed weird jitter in control of FieldCentricDriveTrain
    
    * Cleaned up DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/detection/PropDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/detection/PropDetector.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.modules.detection;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.hardware.ConditionalHardwareDevice;
 import org.firstinspires.ftc.teamcode.modules.core.Module;
 
@@ -33,11 +34,14 @@ public class PropDetector extends Module {
     }
 
     /**
-     * Returns whether the prop is there, based on which RGB value (Red, Green, or Blue) is most present in the sensor's view
+     * Returns whether the prop is there, based on which RGB value (Red, Green, or Blue) is most present in the sensor's view, as well as the distance to the nearest object
      * @param prop This is the prop type to look for
      * @return Whether the prop is there or not as a boolean
      */
     public boolean isPropDetected(Prop prop) {
+        if(sensor.requireDevice().getDistance(DistanceUnit.CM) < 9.5) {
+            return true;
+        }
         switch (prop) {
             case RED_TEAM_PROP:
                 return sensor.requireDevice().red() > sensor.requireDevice().green() && sensor.requireDevice().red() > sensor.requireDevice().blue();
